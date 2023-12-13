04a7a92e15ae6ce5d4e9785cb79c3cd2bdb63de3
Ancr33z <nikita.borisov20011@gmail.com>
2023-12-13 19:58:59 +0300

    .

--- 1sem/15laba/script.js
+++ 1sem/15laba/script.js
@@ -1,13 +1,28 @@
-otpravit.addEventListener("click", () => {
+otpravit = document.querySelector("#otpravit");
+otpravit.addEventListener("click", (event) => {
     event.preventDefault();
-    let familiyavihod = document.getElementById("familiya").value;
-    let imyavihod = document.getElementById("imya").value;
-    let nomertovaravihod = document.getElementById("nomertovara").value;
-    let adresvihod = document.getElementById("adres").value;
+    let familiVavihod = document.getElementById("familiya").value;
+    let imyaVihod = document.getElementById("imya").value;
+    let nomerTovaraVihod = document.getElementById("nomertovara").value;
+    let adresVihod = document.getElementById("adres").value;
+    let footerBlock = document.querySelector("#footerBlock");
     footerBlock.innerHTML = `<div>
-        <p><b>Фамилия:</b>: ${familiyavihod} </p>
-        <p><b>Имя</b>: ${imyavihod}</p>
-        <p><b>Номер товара</b>: ${nomertovaravihod}</p>
-        <p><b>Адрес доставки:</b> ${adresvihod}</p>
+        <p><b>Фамилия</b>: ${familiVavihod} </p>
+        <p><b>Имя</b>: ${imyaVihod}</p>
+        <p><b>Номер товара</b>: ${nomerTovaraVihod}</p>
+        <p><b>Дата доставки:</b> ${adresVihod}</p>
     </div>`;
+
+    event.target.reset();
+
 });
+
+let addBorder = document.querySelector(".secondfoto");
+
+addBorder.addEventListener('click', (event) => {
+    event.preventDefault();
+    addBorder.classList.add("border");
+
+});
+
+console.log(document.getElementsByClassName("container"));
